601a621850f3c00d8ebc3521aa1edb876b5b7ee8
luiz <luizde00@gmail.com>
2024-09-20 19:52:55 -0300

    Mostar mensagem preencha os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -22,20 +23,32 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
         btncalcular.setOnClickListener {
-            val peso: Float = edtpeso.text.toString().toFloat()
-            val altura: Float = edtaltura.text.toString().toFloat()
 
-            val alturaQ2 = altura * altura
-            val resultado = peso / alturaQ2
-            println("Luiz ação do botão" + resultado)
+
+            val pesoStr: String = edtpeso.text.toString()
+            val alturaStr: String = edtaltura.text.toString()
+
+            if (pesoStr == "" || alturaStr == "") {
+                // Mostar mensagem para usuario
+
+                Snackbar
+                    .make(
+                        edtpeso,
+                        "Preencha todos os campos",
+                        Snackbar.LENGTH_LONG
+                    )
+                    .show()
+            } else {
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
+
+                println("Luiz ação do botão" + resultado)
+            }
         }
-
-
-
-
-
 
 
     }
